a595af6b58dec0c7f91c9a46b49d34783743db1b
Brett Smith <brettcsmith@brettcsmith.org>
2014-06-24 07:44:38 -0400

    brettcs: Puddle solution handles left wall slope.

--- solutions/brettcs/puddles.py
+++ solutions/brettcs/puddles.py
@@ -23,17 +23,59 @@ class EndOfPuddle(Exception):
 class Puddle:
     def __init__(self, wall_height, wall_index):
         self.wall_height = wall_height
-        self.start_index = wall_index + 1
-        self.end_index = self.start_index
-        self.volume = 0
+        self._start_index = wall_index + 1
+        self.insides = []
+
+    def _new_end_wall(self, height):
+        try:
+            self.insides.append(self.end_wall_height)
+        except AttributeError:
+            pass
+        self.end_wall_height = height
+        self._end_index = len(self.insides)
+
+    def fits_inside(self, height):
+        if not self.insides:
+            return True
+        try:
+            return height <= self.end_wall_height
+        except AttributeError:
+            return height < max(self.insides)
 
     def add(self, height):
-        height_diff = self.wall_height - height
-        if height_diff > 0:
-            self.volume += height_diff
-            self.end_index += 1
-        else:
+        if height >= self.wall_height:
+            self._new_end_wall(height)
             raise EndOfPuddle()
+        elif self.fits_inside(height):
+            self.insides.append(height)
+        else:
+            self._new_end_wall(height)
+
+    def _start_offset(self):
+        offset = 0
+        for height in self.insides:
+            if height < self.end_wall_height:
+                break
+            offset += 1
+        return offset
+
+    @property
+    def start_index(self):
+        return self._start_index + self._start_offset()
+
+    @property
+    def end_index(self):
+        return self._start_index + self._end_index
+
+    @property
+    def volume(self):
+        try:
+            wall_height = min(self.wall_height, self.end_wall_height)
+        except AttributeError:  # No end wall yet
+            return 0
+        else:
+            return sum(wall_height - height for height in
+                       self.insides[self._start_offset():self._end_index])
 
     def __lt__(self, other):
         return self.volume < other.volume
@@ -50,16 +92,23 @@ def biggest_puddle(heights):
         except EndOfPuddle:
             biggest_puddle = max(puddle, biggest_puddle)
             puddle = Puddle(height, index)
-    return biggest_puddle
+    return max(puddle, biggest_puddle)
 
 
 class PuddleTest(unittest.TestCase):
     def test_basic_puddle(self):
         puddle = Puddle(2, 0)
         puddle.add(1)
-        self.assertEqual(puddle.start_index, 1)
-        self.assertEqual(puddle.volume, 1)
         self.assertRaises(EndOfPuddle, puddle.add, 2)
+        self.assertEqual(puddle.start_index, 1)
+        self.assertEqual(puddle.end_index, 2)
+        self.assertEqual(puddle.volume, 1)
+
+    def test_basic_running_bound_puddle(self):
+        puddle = Puddle(3, 0)
+        puddle.add(1)
+        puddle.add(2)
+        self.assertEqual(puddle.start_index, 1)
         self.assertEqual(puddle.end_index, 2)
         self.assertEqual(puddle.volume, 1)
 
@@ -72,16 +121,58 @@ class PuddleTest(unittest.TestCase):
         self.assertEqual(puddle.start_index, 2)
         self.assertEqual(puddle.end_index, 6)
 
+    def test_example_puddle_reversed(self):
+        puddle = Puddle(7, 2)
+        for height in range(4, 0, -1):
+            puddle.add(height)
+        puddle.add(5)
+        puddle.add(2)
+        self.assertEqual(puddle.volume, 10)
+        self.assertEqual(puddle.start_index, 3)
+        self.assertEqual(puddle.end_index, 7)
+
+    def test_w_puddle(self):
+        puddle = Puddle(4, 0)
+        for height in [1, 2, 1, 3]:
+            puddle.add(height)
+        self.assertEqual(puddle.start_index, 1)
+        self.assertEqual(puddle.end_index, 4)
+        self.assertEqual(puddle.volume, 5)
+
+    def test_ww_puddle(self):
+        puddle = Puddle(4, 0)
+        for height in [1, 3, 1, 3]:
+            puddle.add(height)
+        self.assertEqual(puddle.start_index, 1)
+        self.assertEqual(puddle.end_index, 2)
+        self.assertEqual(puddle.volume, 2)
+
+    def test_staggered_puddle(self):
+        puddle = Puddle(4, 0)
+        for height in [3, 1, 3]:
+            puddle.add(height)
+        self.assertEqual(puddle.start_index, 2)
+        self.assertEqual(puddle.end_index, 3)
+        self.assertEqual(puddle.volume, 2)
+
     def test_no_puddle(self):
         puddle = Puddle(2, 0)
         self.assertRaises(EndOfPuddle, puddle.add, 4)
         self.assertEqual(puddle.volume, 0)
 
+    def test_no_puddle_yet(self):
+        puddle = Puddle(4, 0)
+        for height in range(3, -1, -1):
+            puddle.add(height)
+        self.assertEqual(puddle.volume, 0)
+
     def test_max_puddle(self):
         small_puddle = Puddle(5, 0)
+        small_puddle.add(3)
         small_puddle.add(4)
         big_puddle = Puddle(5, 0)
         big_puddle.add(0)
+        big_puddle.add(4)
         self.assertIs(big_puddle, max(small_puddle, big_puddle))
 
 
@@ -94,6 +185,12 @@ class MeasurePuddleTest(unittest.TestCase):
         puddle = biggest_puddle(self.EXAMPLE)
         self.assertEqual(puddle.start_index, 2)
         self.assertEqual(puddle.end_index, 6)
+        self.assertEqual(puddle.volume, 10)
+
+    def test_reversed_example(self):
+        puddle = biggest_puddle(reversed(self.EXAMPLE))
+        self.assertEqual(puddle.start_index, 3)
+        self.assertEqual(puddle.end_index, 7)
         self.assertEqual(puddle.volume, 10)
 
     def test_pool(self):
